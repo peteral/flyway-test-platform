d5dd02b81b4a843b5f7b9b4c41f6d5cce9e75146
peteral <ladislav.petera@yahoo.de>
2016-03-06 15:12:19 +0100

    added a test

---
+++ platform/platform-war/src/test/java/peteral/test/build/platformwar/MyTest.java
@@ -0,0 +1,12 @@
+package peteral.test.build.platformwar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+	@Test
+	public void aTest() throws Exception {
+		
+	}
+}
