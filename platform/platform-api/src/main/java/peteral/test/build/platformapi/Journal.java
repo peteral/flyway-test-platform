5b7c8e41df1758213cf71f8e3006697aaa023f9e
peteral <ladislav.petera@yahoo.de>
2016-03-06 16:02:06 +0100

    made lookup part of journal API

--- platform/platform-api/src/main/java/peteral/test/build/platformapi/Journal.java
+++ platform/platform-api/src/main/java/peteral/test/build/platformapi/Journal.java
@@ -2,4 +2,6 @@ package peteral.test.build.platformapi;
 
 public interface Journal {
 	void log(String message);
+	
+	String LOOKUP = "java:global/platform-war/JournalBean!peteral.test.build.platformapi.Journal";
 }
